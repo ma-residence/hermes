bca735cc00b80fa7f61c137a6ec667c86de5b641
Sinane <sin.sim@outlook.fr>
2021-06-23 16:01:25 +0200

    head scripts

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import * as queryString from "query-string";
 import "./App.css";
 import FirstPage from "./FirstPage";
 import FourthPage from "./FourthPage";
@@ -6,12 +7,14 @@ import SecondPage from "./SecondPage";
 import ThirdPage from "./ThirdPage";
 
 function App() {
+  const city = queryString(window.location.search);
+  console.log({ city });
   return (
     <div className="App">
       <Header />
       <FirstPage />
       <SecondPage />
-      <div className="spacer gradient"/>
+      <div className="spacer gradient" />
       <ThirdPage />
       <FourthPage />
     </div>
